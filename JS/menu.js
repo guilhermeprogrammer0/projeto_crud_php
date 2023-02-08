bf5868c53ef784444e63c7897a4c5793c87e8d3e
Guilherme <guilherme003souza@gmail.com>
2023-02-08 15:37:06 -0300

    Mobile

---
+++ JS/menu.js
@@ -0,0 +1,7 @@
+var btnToggle = document.querySelector("#btnToggle");
+btnToggle.addEventListener('click',menu_mobile);
+var menu = document.querySelector("#menu");
+
+function menu_mobile(){
+    menu.classList.toggle("menu_ativo")
+}
